0709987e47bca2a3da8105ff26c0871542747711
DmityFilchev <filchev_dmitry@mail.ru>
2022-09-16 14:53:26 +0300

    Добавьте файлы проекта.

---
+++ Task22_2/Task22_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include<string>
+#include<map>
+
+int main()
+{
+    std::string name;
+    int que = 0;
+    bool exit = true;
+    std::map <std::string, std::string> registry;
+   
+    while (exit)
+    { 
+        std::cout << "Enter the last name or next to output (to exit- enter exit)\n";
+        std::cin >> name;
+        if (name == "exit")
+        {
+            exit = false;
+        }
+        else if (name == "next")
+        {
+            std::cout << "<- " << registry.begin()->second << std::endl;
+            registry.erase(registry.begin());
+        }
+        else
+        {
+            registry.insert(std::make_pair(name+std::to_string(que), name));
+            que++;
+        }
+    }
+}
+
